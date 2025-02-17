937f5904a5c3dd5de3fc74faff9379741c5f240e
«Olya-Skr» <ollisha.iz.mg@gmail.com>
2025-02-17 20:46:49 +0300

    Fixed homework №5

--- homework/hw5/README.md
+++ homework/hw5/README.md
@@ -1,37 +1,48 @@
-Домашняя работа
+# Домашняя работа №5:
+
 Реализовать минимум 5 регрессоров, сравнить метрики между собой, выбрать лучший для Вашего датасета.
 
-Регрессоры:
-✓ Регрессор Gradient Boosting.
-✓ Регрессор LGBM.
-✓ Экстремальный градиентный бустинг.
-✓ Гребневая регрессия. 
-✓ Регрессор CatBoost.
-✓ Регрессия Лассо.
 
-Датасет: employee_survey.csv Опрос персонала для оценки удовлетворенности работой
-Колонки:
-EmpID - Уникальный идентификатор для каждого сотрудника
-Gender - Пол (e.g., Male, Female, Other)
-Age - Возраст 
-MaritalStatus - Семейное положение (e.g., Single, Married, Divorced, Widowed)
-JobLevel - Уровень должности сотрудника (e.g., Intern/Fresher, Junior, Mid, Senior, Lead)
-Experience - Cтаж работы
-Dept - Подразделение, в котором работает сотрудник (e.g., IT, HR, Finance, Marketing, Sales, Legal, Operations, Customer Service)
-EmpType - Тип занятости (e.g., Full-Time, Part-Time, Contract)
-WLB - Рейтинг баланса между работой и личной жизнью (scale from 1 to 5)
-WorkEnv - Рейтинг рабочей среды (scale from 1 to 5)
-PhysicalActivityHours - Количество часов физической активности в неделю
-Workload - Рейтинг рабочей нагрузки (scale from 1 to 5)
-Stress - Рейтинг уровня стресса (scale from 1 to 5)
-SleepHours - Количество часов сна в сутки
-CommuteMode - Способ передвижения (e.g., Car, Public Transport, Bike, Walk, Motorbike)
-CommuteDistance - Расстояние (km).
-NumCompanies - Количество различных компаний, в которых работал сотрудник
-TeamSize - Размер команды, частью которой является сотрудник
-NumReports - Количество отчетности (только для уровней Senior и Lead)
-EduLevel - Уровень образования, достигнутый сотрудником (e.g., High School, Bachelor, Master, PhD)
-haveOT - Индикатор наличия у сотрудника сверхурочной работы (True/False)
-TrainingHoursPerYear - Количество часов обучения, полученных в год
-JobSatisfaction - Рейтинг удовлетворенности работой (scale from 1 to 5).
- 
+## Датасет:
+
+_employee_survey.csv_ содержит результат годового опроса персонала в том числе с оценкой удовлетворенности работой.
+
+Колонки:  
+_**EmpID**_ - Уникальный идентификатор для каждого сотрудника  
+_**Gender**_  - Пол (e.g., Male, Female, Other)  
+_**Age**_  - Возраст   
+_**MaritalStatus**_  - Семейное положение (e.g., Single, Married, Divorced, Widowed)  
+_**JobLevel**_  - Уровень должности сотрудника (e.g., Intern/Fresher, Junior, Mid, Senior, Lead)  
+_**Experience**_  - Cтаж работы  
+_**Dept**_  - Подразделение, в котором работает сотрудник (e.g., IT, HR, Finance, Marketing, Sales, Legal, Operations, Customer Service)  
+_**EmpType**_  - Тип занятости (e.g., Full-Time, Part-Time, Contract)  
+_**WLB**_  - Рейтинг баланса между работой и личной жизнью (scale from 1 to 5)  
+_**WorkEnv**_  - Рейтинг рабочей среды (scale from 1 to 5)  
+_**PhysicalActivityHours**_  - Количество часов физической активности в неделю  
+_**Workload**_  - Рейтинг рабочей нагрузки (scale from 1 to 5)  
+_**Stress**_  - Рейтинг уровня стресса (scale from 1 to 5)  
+_**SleepHours**_  - Количество часов сна в сутки  
+_**CommuteMode**_  - Способ передвижения (e.g., Car, Public Transport, Bike, Walk, Motorbike)  
+_**CommuteDistance**_  - Расстояние (km)  
+_**NumCompanies**_  - Количество различных компаний, в которых работал сотрудник  
+_**TeamSize**_  - Размер команды, частью которой является сотрудник  
+_**NumReports**_  - Количество отчетности (только для уровней Senior и Lead)  
+_**EduLevel**_  - Уровень образования, достигнутый сотрудником (e.g., High School, Bachelor, Master, PhD)  
+_**haveOT**_  - Индикатор наличия у сотрудника сверхурочной работы (True/False)  
+_**TrainingHoursPerYear**_  - Количество часов обучения, полученных в год  
+_**JobSatisfaction**_  - Рейтинг удовлетворенности работой (scale from 1 to 5).  
+
+## Цель для регриссинного анализа:
+Предсказание потенциальной длительности обучения сотрудника в год, которое потребуется выделить компании.  
+Целевой меткой для предсказания выбрана _**TrainingHoursPerYear**_
+
+## Модели машинного обучения:
+✓ Регрессор Gradient Boosting  
+✓ Регрессор LGBM  
+✓ Экстремальный градиентный бустинг  
+✓ Гребневая регрессия   
+✓ Регрессор CatBoost  
+✓ Регрессия Лассо  
+
+## Вывод
+Заключение по оценке точности моделей, представленой в ноутбуке _dz5.ipynb_, говорит о том, что наилучшей моделью для выполнения указанной задачи является регрессор **LightGBM**.
--- homework/hw5/main.py
+++ homework/hw5/main.py
@@ -16,49 +16,75 @@ print("\nПропущенные значения:")
 print(df.isnull().sum())
 df = df.drop_duplicates()
 
-# Разделение данных на набор зависимых переменных и метку
-data = df.drop(["JobSatisfaction"],axis=1)
-target = df["JobSatisfaction"]
+# Масштабирование признаков: PhysicalActivityHours, SleepHours
+data_for_scaling=df[["PhysicalActivityHours","SleepHours"]]
+# Нормализация
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler.fit(data_for_scaling)
+data_for_scaling = scaler.transform(data_for_scaling)
+data_for_scaling_new = pd.DataFrame(data_for_scaling,columns=['PhysicalActivityHours', 'SleepHours'])
+data_for_scaling_new.head()
 
-# Вывод столбцов с категориальными признаками
-category_df = data.select_dtypes(include='object')
+# Формируем датасет с обновленными полями
+df = df.drop(['PhysicalActivityHours','SleepHours'],axis=1)
+df=pd.concat([df,data_for_scaling_new],axis=1)
+df.head()
+
+# Обработка категориальных признаков
+category_df = df.select_dtypes(include='object')
 category_df.head()
 
-# Кодирование данных
-categorical_features = category_df.columns.tolist()
-print('Столбцы для кодирования:\n',categorical_features)
-
-from sklearn.preprocessing import LabelEncoder
-encoder = LabelEncoder()
-for each in categorical_features:
-    category_df[each] = encoder.fit_transform(category_df[each])
-    category_df_check=encoder.classes_
-
-numeric_df = data.select_dtypes(include='number')
+# Кодирование данных через OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+encoder_ohe = OneHotEncoder(handle_unknown='ignore',sparse_output=False).set_output(transform='pandas')
+data_ohe = pd.DataFrame()
+data_ohe=pd.concat([data_ohe,encoder_ohe.fit_transform(category_df)],axis=1)
+data_ohe.head()
 
 # Новый набор данных с закодировннными категориями
-new_data =pd.merge(numeric_df, category_df, left_index=True, right_index=True)
-new_df=pd.merge(new_data, target, left_index=True, right_index=True)
+categorical_features = category_df.columns.tolist()
+print('Закодированные столбцы:\n',categorical_features)
+print('\nОбновленный датасет:')
+new_df=pd.concat([df,data_ohe],axis=1).drop(columns=categorical_features)
 new_df.head()
+
+# Для просмотра корреляции попроще закодируем данные через LabelEncoder
+from sklearn.preprocessing import LabelEncoder
+encoder_le = LabelEncoder()
+for each in categorical_features:
+    category_df[each] = encoder_le.fit_transform(category_df[each])
+    category_df_check=encoder_le.classes_
+
+numeric_df = df.select_dtypes(include='number')
+new_df_cor =pd.merge(numeric_df, category_df,  left_index=True, right_index=True)
+new_df_cor.head()
 
 # Кореляция переменных
 plt.figure(figsize=(20, 8))
-sns.heatmap(new_df.corr(), annot=True, cmap='coolwarm')
+sns.heatmap(new_df_cor.corr(), annot=True, cmap='coolwarm')
 plt.title('Correlation Heatmap')
 plt.show()
 
-# Убираем некоторые переменные
-X=new_data.drop(["EmpID","Age","CommuteDistance","NumCompanies","TeamSize","NumReports","TrainingHoursPerYear"],axis=1)
+# Разделение данных на набор зависимых переменных и метку
+data = new_df.drop(["TrainingHoursPerYear"],axis=1)
+target = new_df["TrainingHoursPerYear"]
+
+# Убираем бесполезные переменные
+X=data.drop(["EmpID","CommuteDistance","NumCompanies"],axis=1)
 Y=target
 
 # Проверка распределения целевой метки
-result_target = df.groupby('JobSatisfaction', observed=False)['JobSatisfaction'].count()
+kol_target=Y.nunique()
+print('Количество уникальных:\n',kol_target)
+result_target = new_df.groupby('TrainingHoursPerYear', observed=False)['TrainingHoursPerYear'].count()
 print('\nПроверка распределения целевой метки:\n',result_target)
-plt.hist(target)
-plt.xlabel('Числовые значения')
+plt.hist(target,bins=10)
+plt.xlabel('Значение метки')
 plt.ylabel('Количество')
 plt.title('')
 plt.show()
+
 
 # Градиентный бустинг
 from sklearn.ensemble import GradientBoostingRegressor
@@ -66,33 +92,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Разделение данных на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.05, random_state=177)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=43,stratify=Y)
 
 # Создание и обучение модели Gradient Boosting Regressor
-model = GradientBoostingRegressor(max_depth = 4,n_estimators = 222,loss='squared_error', random_state=0) # параметры
-model.fit(X_train, y_train)
+GBR_model = GradientBoostingRegressor(max_depth = 2,n_estimators = 50) # параметры
+GBR_model.fit(X_train, y_train)
 
 # Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
+GBR_y_pred = GBR_model.predict(X_test)
 
 # Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-r2 = r2_score(y_test, y_pred)
+GBR_mae = mean_absolute_error(y_test, GBR_y_pred)
+GBR_mse = mean_squared_error(y_test, GBR_y_pred)
+GBR_r2 = r2_score(y_test, GBR_y_pred)
 
-print(f'*****************')
-print(f'Метрики качества:')
-print(f'*****************')
-print(f'MAE: {mae:.4f}')
-print(f'MSE: {mse:.4f}')
-print(f'RMSE: {rmse:.4f}')
-print(f'R²: {r2:.4f}')
+print(f'MAE: {GBR_mae:.4f}') # средняя абсолютная ошибка
+print(f'MSE: {GBR_mse:.4f}') # среднюю квадратическую ошибк
+print(f'R²: {GBR_r2:.4f}') # коэффициент детерминации
 
 # Визуализация предсказанных и фактических значений
 plt.figure(figsize=(20, 10))
 plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
+plt.scatter(range(len(GBR_y_pred)), GBR_y_pred, color='red', label='Предсказанные значения')
 plt.xlabel('Наблюдение')
 plt.ylabel('Значение')
 plt.title('Фактические и предсказанные значения')
@@ -100,29 +121,29 @@ plt.legend()
 plt.show()
 
 
-# Регрессор Light GBM без подбора параметров
+# Регрессор Light GBM
 from lightgbm import LGBMRegressor
 
 # Создание и обучение модели LGBMRegressor
-model = LGBMRegressor(random_state=0,force_col_wise='true')
-model.fit(X_train, y_train)
+LGBMR_model = LGBMRegressor(max_depth=-1,n_estimators=50,force_col_wise='true')
+LGBMR_model.fit(X_train, y_train)
 
 # Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
+LGBMR_y_pred = LGBMR_model.predict(X_test)
 
 # Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+LGBMR_mae = mean_absolute_error(y_test, LGBMR_y_pred)
+LGBMR_mse = mean_squared_error(y_test, LGBMR_y_pred)
+LGBMR_r2 = r2_score(y_test, LGBMR_y_pred)
 
-print(f'MAE: {mae}')
-print(f'MSE: {mse}')
-print(f'R²: {r2}')
+print(f'MAE: {LGBMR_mae}')
+print(f'MSE: {LGBMR_mse}')
+print(f'R²: {LGBMR_r2}')
 
 # Визуализация предсказанных и фактических значений
 plt.figure(figsize=(20, 6))
 plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
+plt.scatter(range(len(LGBMR_y_pred)), LGBMR_y_pred, color='red', label='Предсказанные значения')
 plt.xlabel('Наблюдение')
 plt.ylabel('Значение')
 plt.title('Фактические и предсказанные значения')
@@ -134,52 +155,53 @@ plt.show()
 from xgboost import XGBRegressor
 
 # Определение модели
-model = XGBRegressor(max_depth = 3,  n_estimators = 222)
-model.fit(X_train, y_train)
+XGB_model = XGBRegressor(max_depth=3, n_estimators=50)
+XGB_model.fit(X_train, y_train)
 
 # Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
+XGB_y_pred = XGB_model.predict(X_test)
 
 # Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+XGB_mae = mean_absolute_error(y_test, XGB_y_pred)
+XGB_mse = mean_squared_error(y_test, XGB_y_pred)
+XGB_r2 = r2_score(y_test, XGB_y_pred)
 
-print(f'MAE: {mae}')
-print(f'MSE: {mse}')
-print(f'R²: {r2}')
+print(f'MAE: {XGB_mae}')
+print(f'MSE: {XGB_mse}')
+print(f'R²: {XGB_r2}')
 
 # Визуализация предсказанных и фактических значений
 plt.figure(figsize=(20, 6))
 plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
+plt.scatter(range(len(XGB_y_pred)), XGB_y_pred, color='red', label='Предсказанные значения')
 plt.xlabel('Наблюдение')
 plt.ylabel('Значение')
 plt.title('Фактические и предсказанные значения')
 plt.legend()
 plt.show()
 
-#Гребневая регрессия (ридж-регрессия)
+
+#Гребневая регрессия (L2-регуляризацией)
 from sklearn.linear_model import RidgeCV
-model = RidgeCV()
-model.fit(X_train, y_train)
+L2_model = RidgeCV(alphas=10.0) #0.1, 1.0, 10.0
+L2_model.fit(X_train, y_train)
 
 #Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
+L2_y_pred =L2_model.predict(X_test)
 
 # Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+L2_mae = mean_absolute_error(y_test, L2_y_pred)
+L2_mse = mean_squared_error(y_test, L2_y_pred)
+L2_r2 = r2_score(y_test, L2_y_pred)
 
-print(f'MAE: {mae}')
-print(f'MSE: {mse}')
-print(f'R²: {r2}')
+print(f'MAE: {L2_mae}')
+print(f'MSE: {L2_mse}')
+print(f'R²: {L2_r2}')
 
 # Визуализация предсказанных и фактических значений
 plt.figure(figsize=(20, 10))
 plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
+plt.scatter(range(len(L2_y_pred)), L2_y_pred, color='red', label='Предсказанные значения')
 plt.xlabel('Наблюдение')
 plt.ylabel('Значение')
 plt.title('Фактические и предсказанные значения')
@@ -187,69 +209,79 @@ plt.legend()
 plt.show()
 
 
-# Регрессор catboost
-from catboost import CatBoostRegressor
-
-# Создание и обучение модели CatBoost для регрессии
-model = CatBoostRegressor(iterations=100, learning_rate=0.05, depth=6, random_state=123, verbose=0)
-model.fit(X_train, y_train)
-
-#Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
-
-# Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print(f'MAE: {mae}')
-print(f'MSE: {mse}')
-print(f'R²: {r2}')
-
-# Визуализация предсказанных и фактических значений
-plt.figure(figsize=(20, 10))
-plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
-plt.xlabel('Наблюдение')
-plt.ylabel('Значение')
-plt.title('Фактические и предсказанные значения')
-plt.legend()
-plt.show()
-
-# Регрессия LASSO
+# Регрессия LASSO (L1-регуляризация)
 from sklearn.linear_model import Lasso
 
 # Создание и обучение модели регрессии Lasso
-model = Lasso(alpha=0.1)  # Здесь alpha - гиперпараметр регуляризации L1
-model.fit(X_train, y_train)
+L1_model = Lasso(alpha=0.1)  # Здесь alpha - гиперпараметр регуляризации L1
+L1_model.fit(X_train, y_train)
 
 #Предсказание на тестовой выборке
-y_pred = model.predict(X_test)
+L1_y_pred = L1_model.predict(X_test)
 
 # Вычисление метрик
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+L1_mae = mean_absolute_error(y_test, L1_y_pred)
+L1_mse = mean_squared_error(y_test, L1_y_pred)
+L1_r2 = r2_score(y_test, L1_y_pred)
 
-print(f'MAE: {mae}')
-print(f'MSE: {mse}')
-print(f'R²: {r2}')
+print(f'MAE: {L1_mae}')
+print(f'MSE: {L1_mse}')
+print(f'R²: {L1_r2}')
 
 # Визуализация предсказанных и фактических значений
 plt.figure(figsize=(20, 10))
 plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
-plt.scatter(range(len(y_test)), y_pred, color='red', label='Предсказанные значения')
+plt.scatter(range(len(L1_y_pred)), L1_y_pred, color='red', label='Предсказанные значения')
 plt.xlabel('Наблюдение')
 plt.ylabel('Значение')
 plt.title('Фактические и предсказанные значения')
 plt.legend()
 plt.show()
 
-# Визуализация результатов
-plt.figure(figsize=(10, 6))
-plt.scatter(y_test, y_pred, color='blue', edgecolor='k', alpha=0.7)
-plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linewidth=2)
-plt.xlabel("Актуальное значение")
-plt.ylabel("Предсказанное значение")
-plt.title("Актуальное vs. Предсказанное (Lasso Regression)")
+#pip install catboost
+from catboost import CatBoostRegressor
+
+# Создание и обучение модели CatBoost
+CB_model = CatBoostRegressor(iterations=100, learning_rate=0.05, depth=3, random_state=55, verbose=0)
+CB_model.fit(X_train, y_train)
+
+#Предсказание на тестовой выборке
+CB_y_pred = CB_model.predict(X_test)
+
+# Вычисление метрик
+CB_mae = mean_absolute_error(y_test, CB_y_pred)
+CB_mse = mean_squared_error(y_test, CB_y_pred)
+CB_r2 = r2_score(y_test, CB_y_pred)
+
+print(f'MAE: {CB_mae}')
+print(f'MSE: {CB_mse}')
+print(f'R²: {CB_r2}')
+
+# Визуализация предсказанных и фактических значений
+plt.figure(figsize=(20, 10))
+plt.scatter(range(len(y_test)), y_test, color='blue', label='Фактические значения')
+plt.scatter(range(len(CB_y_pred)), CB_y_pred, color='red', label='Предсказанные значения')
+plt.xlabel('Наблюдение')
+plt.ylabel('Значение')
+plt.title('Фактические и предсказанные значения')
+plt.legend()
 plt.show()
+
+
+# Сравнение метрик качества моделей
+print(f'Сравнение метрик качества моделей:')
+results_metrics = pd.DataFrame([
+  ['Градиентный бустинг XGBoost', GBR_mae,GBR_mse,GBR_r2],
+  ['Light_GBM', LGBMR_mae,LGBMR_mse,LGBMR_r2],
+  ['Экстремальный градиентный бустинг XGBoost', XGB_mae,XGB_mse,XGB_r2],
+  ['Гребневая регрессия L2', L2_mae,L2_mse,L2_r2],
+  ['Регрессия LASSO L1', L1_mae,L1_mse,L1_r2],
+  ['CatBoost', CB_mae,CB_mse,CB_r2]],
+  columns=['model','mae', 'mse','r2',])
+display(results_metrics)
+
+# Визуализируем ошибки
+from matplotlib import pyplot as plt
+import seaborn as sns
+results_metrics.plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
+plt.gca().spines[['top', 'right',]].set_visible(False)
